90627432ee7aee19ed0468314860dca0e9bed9ef
menor <menor@LAPTOP-DRHH4VNE>
2023-04-26 18:50:08 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentModule.Models;
+using PaymentModule.Services;
+using System.Data;
+using System.Diagnostics;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace PaymentModule.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISqlService _sqlService;
+
+        public HomeController(ISqlService sqlService)
+        {
+            _sqlService = sqlService;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var strSql = "Select * from dbo.Biller";
+            DataTable data = _sqlService.Query(strSql);
+            List<BillerModel> biller = new List<BillerModel>();
+            foreach(DataRow item in data.Rows)
+            {
+                biller.Add(new BillerModel() { transaction_id = Convert.ToInt32(item["transaction_id"]), biller_name = item["biller_name"].ToString(), amount_to_pay = Convert.ToInt32(item["amount_to_pay"]), transaction_type = item["transaction_type"].ToString() }) ;
+            }
+            return View(biller);
+        }
+
+        // ignore
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Services/SqlService.cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer;
+
+
+namespace PaymentModule.Services
+{
+    public class SqlService : ISqlService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SqlService(IConfiguration configuration) { 
+            _configuration = configuration;
+        }
+
+        public DataTable Query(string sql)
+        {
+            try
+            {
+                var sqlString = _configuration.GetConnectionString("connString");
+                using (SqlConnection connection = new SqlConnection(sqlString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using(SqlDataAdapter adapter= new SqlDataAdapter(command))
+                        {
+                            DataTable dTable = new DataTable();
+                            adapter.Fill(dTable);
+                            connection.Close();
+                            return dTable;
+                        }
+                    }
+                }   
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+
+    public interface ISqlService
+    {
+        DataTable Query(string sql);
+    }
+}
